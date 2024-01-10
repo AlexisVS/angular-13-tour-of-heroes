540d265a953163133e11bdab1768b95741fa95f5
AlexisVS <alexis.vansan1440@gmail.com>
2024-01-10 10:55:02 +0100

    before try to install angular-in-memory-web-api

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,16 +3,16 @@ import {Hero} from "../hero/hero";
 import {HeroService} from "../hero/hero.service";
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class DashboardComponent implements OnInit {
 
   public heroes: Hero[] = [];
 
   constructor(
-    private heroService: HeroService,
+    private heroService: HeroService
   ) {
   }
 
@@ -23,6 +23,6 @@ export class HeroesComponent implements OnInit {
   public getHeroes(): void {
     this.heroService
       .getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
   }
 }
